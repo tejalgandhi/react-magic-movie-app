0bcf99a4fd1e803f1675f08c6db6e8deffbdf6d9
tejal <tejal@enstead.com>
2024-04-11 14:53:52 +0530

    first-commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <nav className="navbar">Navbar content</nav>
+      <main className="main">Main content</main>
+    </div>
+  );
+};
+
+export default App;
